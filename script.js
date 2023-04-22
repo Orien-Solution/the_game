7abb97e301ab48d5c0a9fc4c58bbc76c8fe2a076
Sulamaxx <sjeewaranga@gmail.com>
2023-04-23 01:22:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ function WoodcutterAnimationStart() {
 }
 
 var attack1Oness = 0;
+var attack2Oness = 0;
+var attack3Oness = 0;
 var idleOness = 0;
 var walkOness = 0;
+var climbOness = 0;
 
 function KeyLisiner(event) {
     var KeyCode = event.which;
@@ -27,20 +30,51 @@ function KeyLisiner(event) {
     if (KeyCode == 69) {
         //E
         if (attack1Oness == 0) {
+
+            attack2Oness = 0;
+            attack3Oness = 0;
             idleOness = 0;
             walkOness = 0;
+            climbOness = 0;
             attack1Oness = 1;
             WoodcutterAttck1Animation();
         }
 
 
+    } else if (KeyCode == 81) {
+        if (attack2Oness == 0) {
+            attack1Oness = 0;
+
+            attack3Oness = 0;
+            idleOness = 0;
+            walkOness = 0;
+            climbOness = 0;
+            attack2Oness = 1;
+            WoodcutterAttack2Animation();
+        }
     } else if (KeyCode == 83) {
         //S
         if (idleOness == 0) {
             attack1Oness = 0;
+            attack2Oness = 0;
+            attack3Oness = 0;
+
             walkOness = 0;
+            climbOness = 0;
             idleOness = 1;
             WoodcutterIdleAnimation();
+        }
+    } else if (KeyCode == 88) {
+        //X
+        if (attack3Oness == 0) {
+            attack1Oness = 0;
+            attack2Oness = 0;
+
+            idleOness = 0;
+            walkOness = 0;
+            climbOness = 0;
+            attack3Oness = 1;
+            WoodcutterAttck3Animation();
         }
     } else if (KeyCode == 65) {
         //A
@@ -48,14 +82,33 @@ function KeyLisiner(event) {
     } else if (KeyCode == 68) {
         //D
         if (walkOness == 0) {
-            idleOness = 0;
             attack1Oness = 0;
+            attack2Oness = 0;
+            attack3Oness = 0;
+            idleOness = 0;
+
+            climbOness = 0;
             walkOness = 1;
-            WoodcutterWalkAnimation()
+            WoodcutterWalkAnimation();
         }
     } else if (KeyCode == 87) {
         //W
+        if (climbOness == 0) {
+            attack1Oness = 0;
+            attack2Oness = 0;
+            attack3Oness = 0;
+            idleOness = 0;
+            walkOness = 0;
+
+            climbOness = 1;
+            WoodcutterClimbAnimation();
+        }
+
     }
+}
+
+function WoodcutterClimbAnimation() {
+    Woodcutter.style.backgroundImage = "url(resources/Character/Woodcutter/Woodcutter_climb.png)";
 }
 
 function WoodcutterAttck1Animation() {
@@ -64,11 +117,19 @@ function WoodcutterAttck1Animation() {
 
 }
 
+function WoodcutterAttck3Animation() {
+
+    Woodcutter.style.backgroundImage = "url(resources/Character/Woodcutter/Woodcutter_attack3.png)";
+
+}
+
+
+
 function WoodcutterAttck1BackAnimation() {
 
     Woodcutter.style.backgroundImage = "url(resources/Character/Woodcutter/Woodcutter_walk.png)";
     alert("ok");
-
+    ///back scale(-)
     alert("ok1");
 }
 
