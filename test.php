c90a05ce7b0e119617b59231df1d7a7f3970eb4c
Sulamaxx <sjeewaranga@gmail.com>
2023-04-22 20:36:12 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,23 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body onkeyup="KeyLisiner(event);">
+
+    <div class=" col-12 p-5">
+        <div class="row mt-3 mb-3">
+
+            <div class="Woodcutter" id="Woodcutter"></div>
+
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
