79c308dae6f8b39790c797c72f09756d098f8ac6
Sulamaxx <sjeewaranga@gmail.com>
2023-04-22 20:36:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+var Woodcutter = document.getElementById("Woodcutter");
+
+var WoodcutterBackgroundPositionX = 0;
+var WoodcutterAttack1AnimationId = 0;
+
+
+function WoodcutterAttack1Animation() {
+    WoodcutterBackgroundPositionX = WoodcutterBackgroundPositionX - 48;
+    Woodcutter.style.backgroundPositionX = WoodcutterBackgroundPositionX = "px";
+}
+
+function WoodcutterAttack1AnimationStart() {
+    WoodcutterAttack1AnimationId = setInterval(WoodcutterAttack1Animation, 100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function KeyLisiner(event) {
+    var KeyCode = event.which;
+    // alert(KeyCode);
+    if (KeyCode == 68) {
+        WoodcutterAttack1AnimationStart();
+
+    }
+}
