50ac9d5fd9a14badabc00955d498ffd7ce9b037e
Sulamaxx <sjeewaranga@gmail.com>
2023-04-22 18:34:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,51 +13,75 @@
 
 <body style="background-image: radial-gradient(circle,#F9E79F ,#FAD7A0,#F0B27A);">
 
-    <div class="container-fluid" style="width: 100%; height: 100vh;">
+    <div class="container-fluid" style="width: 100%;">
         <div class="row justify-content-center">
 
-            <div class="col-6 bg bg-light text-center col-12">
-                <h2 class="col-12 fs-1 fw-bold">The Game Name</h2>
+            <div class="col-12 bg bg-secondary text-center">
+                <h2 class="col-12 fs-1 fw-bold text-white">The Game Name</h2>
             </div>
 
-            <div class="container">
+            <div class="col-12 mb-3">
                 <div class="row">
-                    <div class="col-6 col-lg-3">
-                        <div class="row text-center">
-                            <h3 class="">Menu</h3>
-                            <div class="border border-dark rounded"></div>
+
+
+
+                    <div class="col-12 col-lg-3 mt-3 ">
+                        <div class="row gap-2 p-2 align-items-center">
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                            <button class="col-12">Button1</button>
+                        </div>
+
+                    </div>
+
+
+
+
+                    <div class="col-12 col-lg-6 mt-3">
+                        <div class="row text-center justify-content-center">
+                            <div class="bg-info rounded border border-danger col-12">
+                                <div class="row justify-content-center">
+                                    <img class="col-10 mt-5" src="resources/Character/Free-Swamp-Game-Tileset-Pixel-Art-720x480.jpg" alt="">
+                                    <button class=" col-8 btn btn-primary fs-1 fw-bold m-4">Play</button>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
-                    <div class="d-lg-none d-block col-6">
+                    <div class="col-12 col-lg-3 mt-3">
                         <div class="row">
-                            <h3>Description</h3>
-                            <div class="border border-dark rounded"></div>
+
+                            <h3 class="text-center fw-bold col-12">Description</h3>
+                            <div class="col-12">
+                                <p class="mt-3">Examples of Descriptive Writing
+
+                                    Her last smile to me wasn’t a sunset. It was an eclipse, the last eclipse, noon dying away to darkness where there would be no dawn.
+                                    My Uber driver looked like a deflating airbag and sounded like talk radio on repeat.
+                                    The old man was bent into a capital C, his head leaning so far forward that his beard nearly touched his knobby knees.</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="col-12 col-lg-6">
-                        <div class="row text-center">
-                            <h3>Container</h3>
-                            <div class="bg-info rounded border border-danger vh-100"></div>
-                        </div>
-                    </div>
 
-                    <div class="col-lg-3 d-none d-lg-block">
-                        <div class="row text-center">
-                            <h3>Description</h3>
-                            <div class="border border-dark rounded"></div>
-                        </div>
-                    </div>
 
 
 
                 </div>
             </div>
 
+
+
+
         </div>
     </div>
-
+    <footer class="col-12 m-0 vw-100">
+        <div class="col-12 bg-secondary" style="height: 100px;"></div>
+    </footer>
 </body>
 
 </html>
